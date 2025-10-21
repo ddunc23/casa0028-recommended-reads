971de61deffcc29d14f23ad9ce28a8e5ba53543a
ddunc23 <d.hay@ucl.ac.uk>
2025-10-21 14:26:17 +0100

    new branch for the actual building of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './tw-styles.css'
 import TitleBar from './components/TitleBar'
 
 function App() {
@@ -13,29 +13,9 @@ function App() {
   }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <TitleBar />
-        <button onClick={increment}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="mx-auto max-w-screen-xl bg-gray-50 min-h-screen">
+      <TitleBar />
+    </div>
   )
 }
 
