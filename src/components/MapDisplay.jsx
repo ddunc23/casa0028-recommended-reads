663fad410296c79e3777782fd2804ccadb49ff1f
ddunc23 <d.hay@ucl.ac.uk>
2026-02-02 09:43:22 +0000

    properties on popup

--- src/components/MapDisplay.jsx
+++ src/components/MapDisplay.jsx
@@ -62,7 +62,7 @@ function MapDisplay(props) {
                 <p className="text-xs text-blue-500 my-2"><a href={selectedPlaque.lead_subject_wikipedia}>Wikipedia</a></p>
                 <button 
                     className={"rounded-l-sm border border-gray-200 px-3 py-2 font-medium text-gray-700 transition-colors hover:bg-gray-50 hover:text-gray-900 focus:z-10 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-white focus:outline-none disabled:pointer-events-auto disabled:opacity-50"}
-                    onClick={() => console.log('Fetch recommended reading for', selectedPlaque.lead_subject_name)}
+                    onClick={() => console.log('Fetch recommended reading for', selectedPlaque.properties.lead_subject_name)}
                 >Recommended Reading</button>
             </div>        
         </Popup>
